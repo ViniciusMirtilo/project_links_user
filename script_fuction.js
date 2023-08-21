02ba322b660a1bb2d18426dfe45b849b6fe687ab
ViniciusMirtilo <vinicius.contato899@hotmail.com>
2023-08-21 03:18:43 -0300

    add codes html, css, js

--- script_fuction.js
+++ script_fuction.js
@@ -10,9 +10,23 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     // light on
-    img.setAttribute("src", "./assets/fursona.png")
+    img.setAttribute("src", "./assets/fursona_02.png")
   } else {
     // light off
     img.setAttribute("src", "./assets/Theme=Dark mode.jpg")
+  }
+
+  // pegar tag alt
+
+  const alt = document.querySelector("#profile img")
+
+  // substituir a legenda
+
+  if (html.classList.contains("light")) {
+    // light on
+    alt.setAttribute("alt", "Fursona do Mirtilo")
+  } else {
+    // light off
+    alt.setAttribute("alt", "Foto do vinicius")
   }
 }
